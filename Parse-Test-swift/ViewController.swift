adea74c06a1b07f609d594c5d57fead7d6206e90
Syu <syusan3@icloud.com>
2015-03-14 09:47:22 +0900

    parse最新版 parseに値を入れて、それを取得するのができました。

---
+++ Parse-Test-swift/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  Parse-Test-swift
+//
+//  Created by 加藤 周 on 2015/03/09.
+//  Copyright (c) 2015年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView : UITableView!
+    var pictures: NSArray = NSArray()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.loadData { (pictures, error) -> () in
+            self.pictures = pictures
+            self.tableView.reloadData()
+           
+        }
+        
+        tableView.dataSource=self;
+        tableView.delegate=self;
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func loadData(callback:([PFObject]!, NSError!) -> ())  {
+        NSLog("loadData")
+        var query: PFQuery = PFQuery(className: "monsuta")
+        query.orderByAscending("createdAt")
+        query.findObjectsInBackgroundWithBlock { (objects: [AnyObject]!, error: NSError!) -> Void in
+            if (error != nil){
+                // エラー処理
+            }
+            callback(objects as [PFObject], error)
+            
+        }
+    
+
+    }
+    
+    
+    
+    
+     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return pictures.count
+    }
+    
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return self.pictures.count
+//    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        var imageFile: PFFile? = self.pictures[indexPath.row].objectForKey("Image") as PFFile?
+        imageFile?.getDataInBackgroundWithBlock({ (imageData, error) -> Void in
+            if(error == nil) {
+                cell.imageView!.image = UIImage(data: imageData)
+                cell.textLabel!.text = self.pictures[indexPath.row].objectForKey("ImageName") as? String
+            }
+        })
+        return cell
+    }
+
+    @IBAction func tapAddButton(sender: AnyObject) {
+        var object: PFObject = PFObject(className: "monsuta")
+        object["ImageName"] = "PinkMonster"
+        var imageData: NSData = UIImageJPEGRepresentation(UIImage(named: "pinkukyarakuta.png"), 0.1)
+        var file: PFFile = PFFile(name: "pinkukyarakuta.png", data: imageData)
+        object["Image"] = file
+        object.saveInBackgroundWithBlock { (succeeded, error) -> Void in
+            self.loadData { (pictures, error) -> () in
+                self.pictures = pictures
+                self.tableView.reloadData()
+                
+            }
+
+            self.tableView.reloadData()
+        }
+
+    }
+}
+
